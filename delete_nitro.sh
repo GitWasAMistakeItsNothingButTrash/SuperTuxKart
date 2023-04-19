91d5e85be0ee1122bfb9bcd512045d589806f1ab
GitWasAMistakeItsNothingButTrash <108470862+GitWasAMistakeItsNothingButTrash@users.noreply.github.com>
2023-04-19 01:20:05 +0000

    Update delete_nitro.sh

--- delete_nitro.sh
+++ delete_nitro.sh
@@ -4,7 +4,7 @@ for track in /usr/share/games/supertuxkart/data/tracks/*/scene.xml
     do
         if [[ $track == "/usr/share/games/supertuxkart/data/tracks/sandtrack/scene.xml" ]]
             then :
-        elif [[ $track = "/usr/share/games/supertuxkart/data/tracks/xr591/scene.xml" ]]
+        elif [[ $track == "/usr/share/games/supertuxkart/data/tracks/xr591/scene.xml" ]]
             then :
         else
             gawk -i inplace '!/small-nitro/' $track
